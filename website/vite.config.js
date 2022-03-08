bc70a13cdccbe13ec3620409f2cc3f575bec34c4
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2022-03-08 10:11:10 -0500

    fixing

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,24 +1,24 @@
-const { resolve } = require('path')
-const { defineConfig } = require('vite')
+const { resolve } = require("path");
+const { defineConfig } = require("vite");
 
 /**
  * @type {import('vite').UserConfig}
  */
 module.exports = defineConfig({
-    root: resolve(__dirname, './app'),
-    build: {
-        outDir: resolve(__dirname, './out'),
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, './app/index.html'),
-                about: resolve(__dirname, './app/page/about.html'),
-                projects: resolve(__dirname, './app/page/projects.html'),
-            }
-        },
-        watch: {
-            buildDelay: 0,
-            clearScreen: true,
-            exclude: "node_modules/**",
-        }
-    }
-})
+  root: resolve(__dirname, "./app"),
+  build: {
+    outDir: resolve(__dirname, "./dist"),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./app/index.html"),
+        about: resolve(__dirname, "./app/page/about.html"),
+        projects: resolve(__dirname, "./app/page/projects.html"),
+      },
+    },
+    watch: {
+      buildDelay: 0,
+      clearScreen: true,
+      exclude: "node_modules/**",
+    },
+  },
+});
